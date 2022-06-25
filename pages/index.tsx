ee570b634214c2e125ad3ad446e23d6a8d1a8d52
fkadeal <infkadeal@gmail.com>
2022-06-25 08:34:41 -0700

    remove

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,12 +27,7 @@ const Home: NextPage = () => {
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
+ 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
